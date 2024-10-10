9069dda79678e3b77d2c998355f45bae15c8db54
TimoMangCut <np.tn123@gmail.com>
2024-10-10 13:08:15 +0700

    .

--- Brute_Force_DBs-Tables_Columns/testoop/test1.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include_once("testoop.php");
-        $conca = new conca("Ca keo vui ve ", "mau trang");
-        $result = "day la 1 " . $conca->display() . $conca->get_color();
-        echo $result;
-?>
